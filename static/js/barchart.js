8626b47a9214d2914bcaf82170623ab491194f95
sdl60660 <learnersd@gmail.com>
2020-05-10 15:55:42 -0400

    Barchart class is fully functioning with both on the page at the same time, using a boolean to distiguish if it is representing season groups or episodes. Made some changes to the way that the season labels display and refactored a little bit in barchart.js

--- static/js/barchart.js
+++ static/js/barchart.js
@@ -1,5 +1,4 @@
 
-
 function translateGrade(grade) {
     
     var gradeTranslationDict = {
@@ -21,59 +20,76 @@ function translateGrade(grade) {
     return gradeTranslationDict[grade];
 }
 
+function onlyUnique(value, index, self) { 
+    return self.indexOf(value) === index;
+};
+
 function mouseover() {
-    d3.selectAll(".episode-bar")
-        .attr("opacity", 0.4)
+    var rectClass = this.getAttribute('class').split(' ')[0];
+
+    var val = parseFloat(this.getAttribute('grade'));
+    if(isNaN(val)) {
+        var valueLabel = this.getAttribute('grade');
+    }
+    else {
+        var valueLabel = d3.format('.1f')(100*(this.getAttribute('grade') / 11.0));
+    }  
+
+    d3.selectAll('.' + rectClass)
+        .attr("opacity", 0.4);
     d3.select(this)
         .attr("opacity", 0.9);
 
-    d3.selectAll(".season-label")
-        .attr("opacity", 0.0);
-    d3.select("#season-" + this.getAttribute("season") + "-label")
+    // d3.selectAll(".season-label")
+    //     .attr("opacity", 0.0);
+    d3.selectAll("#season-" + this.getAttribute("season") + "-label")
         .attr("opacity", 1.0);
 
-    d3.select('g').append('text')
+    d3.select('g.' + this.getAttribute("text-group-class")).append('text')
         .attr("x", parseFloat(this.getAttribute("x")) + (parseFloat(this.getAttribute("width"))/2))
         .attr("y", this.getAttribute("y") - 10)
         .attr("text-anchor", "middle")
         .attr("class", "grade-hover-text")
         .attr("color", "black")
-        .text(this.getAttribute('grade'));
+        .text(valueLabel);
 }
 
 function mouseout() {
+    var rectClass = this.getAttribute('class').split(' ')[0];
     d3.selectAll(".grade-hover-text")
         .remove();
 
     d3.selectAll(".season-label")
-        .attr("opacity", 1.0);
+        .attr("opacity", 0.0);
 
-    d3.selectAll(".episode-bar")
+    d3.selectAll('rect')
         .attr("opacity",defaultFillOpacity);
 }
 
 var defaultFillOpacity = 0.7;
 
-BarChart = function(_parentElement, _fullData) {
+BarChart = function(_parentElement, _fullData, _dimensions, _rectClass, _seasonChart) {
     this.parentElement = _parentElement;
     this.fullData = _fullData;
-    // this.yVariable = _yVariable;
+    this.dimensions = _dimensions;
+    this.rectClass = _rectClass;
+    this.seasonChart = _seasonChart;
 
-    this.initVis(this.fullData);
+    this.initVis();
 }
 
-BarChart.prototype.initVis = function(fullData) {
+BarChart.prototype.initVis = function() {
     var vis = this;
 
-    vis.season_numberColor = d3.scaleOrdinal()
+    vis.seasonColor = d3.scaleOrdinal()
       .domain([0,100])
       .range(d3.schemePaired);
 
 
     // set the dimensions and margins of the graph
     vis.margin = {top: 40, right: 30, bottom: 30, left: 50},
-        vis.width = 800 - vis.margin.left - vis.margin.right,
-        vis.height = 600 - vis.margin.top - vis.margin.bottom;
+        vis.width = vis.dimensions[0] - vis.margin.left - vis.margin.right,
+        vis.height = vis.dimensions[1] - vis.margin.top - vis.margin.bottom;
 
     // append the svg object to the body of the page
     vis.svg = d3.select(vis.parentElement)
@@ -81,7 +97,9 @@ BarChart.prototype.initVis = function(fullData) {
             .attr("width", vis.width + vis.margin.left + vis.margin.right)
             .attr("height", vis.height + vis.margin.top + vis.margin.bottom)
 
+    vis.parentGroupClass = "g-" + vis.parentElement.substring(1)
     vis.g = vis.svg.append("g")
+        .attr("class", vis.parentGroupClass)
         .attr("transform",
               "translate(" + vis.margin.left + "," + vis.margin.top + ")");
 
@@ -109,7 +127,7 @@ BarChart.prototype.initVis = function(fullData) {
         .attr("transform", "translate(-5,0)")
         .call(vis.yAxisCall);
 
-    vis.wrangleData(fullData);
+    vis.wrangleData(vis.fullData);
 }
 
 
@@ -120,32 +138,67 @@ BarChart.prototype.wrangleData = function(fullData) {
     vis.showData = vis.fullData.show
     vis.episodes = vis.fullData.episodes;
 
-    console.log(vis.episodes);
-
     vis.sortedEpisodes = vis.episodes.sort((a, b) => (a.season_number > b.season_number) ? 1 : (a.season_number === b.season_number) ? ((a.episode_number > b.episode_number) ? 1 : -1) : -1 );
     vis.sortedEpisodes =  vis.sortedEpisodes.filter(x => x.letter_grade != null && x.letter_grade.length <= 2);
 
-    for (i=0; i<vis.sortedEpisodes.length; i++) {
-        vis.sortedEpisodes[i].chart_index = i;
-    }
+    vis.lastSeason = vis.sortedEpisodes[vis.sortedEpisodes.length - 1].season_number;
 
-    vis.updateVis();
+    if(vis.seasonChart) {
+        vis.seasonFormatting();
+    }
+    else {
+        vis.chartData = vis.sortedEpisodes;
+        vis.updateVis();
+    }
     
+}
+
+BarChart.prototype.seasonFormatting = function() {
+    var vis = this;
+
+    vis.seasonsList = vis.sortedEpisodes.map(function(d) {
+            return d.season_number;
+        }).filter(onlyUnique);
+
+    vis.seasonData = [];
+    vis.seasonsList.forEach(function(seasonNumber) {
+        var seasonGroup = vis.sortedEpisodes.filter(d => d.season_number == seasonNumber);
+
+        var sum = 0;
+        seasonGroup.forEach(function(d) {
+            sum += translateGrade(d.letter_grade);
+        })
+
+        // console.log(seasonGroup);
+        vis.seasonData.push({
+            'season_number': seasonNumber,
+            'reviewed_episodes': seasonGroup.length,
+            'average_score': d3.format(".1f")(1.0*sum / seasonGroup.length),
+            'imdb_episode_id': seasonGroup[0].show_id + '-' + seasonGroup[0].season_number
+        })
+    })
+
+    vis.chartData = vis.seasonData;
+    vis.updateVis();
 }
 
 
 BarChart.prototype.updateVis = function() {
     var vis = this;
 
-    vis.x.domain(vis.sortedEpisodes.map(function(d) {
+    for (i=0; i<vis.chartData.length; i++) {
+        vis.chartData[i].chart_index = i;
+    }
+
+    vis.x.domain(vis.chartData.map(function(d) {
         return d.chart_index;
     }))
     
     vis.linearXScale = d3.scaleLinear()
-    .domain([0, 1 + d3.max(vis.sortedEpisodes, function(d) {
-        return d.chart_index;
-    })])
-    .range([0, vis.width])
+        .domain([0, 1 + d3.max(vis.chartData, function(d) {
+            return d.chart_index;
+        })])
+        .range([0, vis.width])
     // .paddingInner(0.1);
 
     // Add Axes
@@ -157,13 +210,14 @@ BarChart.prototype.updateVis = function() {
             .transition()
             .call(vis.xAxisCall);
 
-    vis.lastSeason = vis.sortedEpisodes[vis.sortedEpisodes.length - 1].season_number;
 
     // JOIN data with any existing elements
-    vis.barChart = vis.g.selectAll("rect")
-        .data(vis.sortedEpisodes, function(d) {
+    vis.barChart = vis.g.selectAll('rect')
+        .data(vis.chartData, function(d) {
             return d.imdb_episode_id;
         })
+        // .data(vis.chartData)
+
 
     // EXIT old elements not present in new data
     vis.barChart.exit()
@@ -190,57 +244,61 @@ BarChart.prototype.updateVis = function() {
                 }) 
                 .attr("width", vis.x.bandwidth)
                 .attr("fill", function(d) {
-                    return vis.season_numberColor(d.season_number);
+                    return vis.seasonColor(d.season_number);
                 })
                 .attr("opacity", defaultFillOpacity)
                 .attr("class", function(d) {
-                    return "episode-bar season_number-" + d.season_number;
+                    return `${vis.rectClass} season-` + d.season_number;
                 })
                 .attr("season", function(d) {
                     return d.season_number;
                 })
                 .attr("grade", function(d) {
-                    return d.letter_grade;
+                    if(vis.seasonChart) {
+                        return d.average_score;
+                    }
+                    else {
+                        return d.letter_grade; 
+                    }
                 })
+                .attr("text-group-class", vis.parentGroupClass)
                 .attr("y", vis.y(0))
                 .attr("height", 0)
-                
                 .transition()
-                    // .delay(function(d,i) {
-                    //     return 400+(i*30);
-                    // })
                     .delay(400)
                     .attr("height", function(d) {
-                        return vis.height - vis.y(translateGrade(d.letter_grade));
+                        if(vis.seasonChart) {
+                            return vis.height - vis.y(d.average_score);
+                        }
+                        else {
+                            return vis.height - vis.y(translateGrade(d.letter_grade));
+                        }
                     })
                     .attr("y", function(d) {
-                        return vis.y(translateGrade(d.letter_grade));
+                        if(vis.seasonChart) {
+                            return vis.y(d.average_score);
+                        }
+                        else {
+                            return vis.y(translateGrade(d.letter_grade));
+                        }
                     })
 
                     .duration(500)
                     
 
-    d3.selectAll('.season-avg-line').remove();
-    d3.selectAll('.season-label').remove();
+    vis.g.selectAll('.season-label').remove();
+    vis.g.selectAll('.season-avg-line').remove();
 
     for (i=1; i<=vis.lastSeason; i++) {
 
-        vis.group = vis.sortedEpisodes.filter((episode) => episode.season_number == i);
+        vis.group = vis.chartData.filter((episode) => episode.season_number == i);
         if (vis.group.length == 0) {
             continue;
         }
 
         vis.average = vis.group.reduce((total, next) => total + translateGrade(next.letter_grade), 0) / vis.group.length;
 
-        vis.seasonLine = vis.g.append("line")
-            .attr("x1", vis.linearXScale(vis.group[0].chart_index))
-            .attr("x2", vis.linearXScale(vis.group[vis.group.length - 1].chart_index + 1))
-            .attr("y1", vis.y(vis.average))
-            .attr("y2", vis.y(vis.average))
-            .attr("class", "season-avg-line")
-            .attr("stroke", "black")
-            .style("stroke-dasharray", ("3, 3"));
-
+        // Add season labels (default to hidden)
         vis.seasonLabel = vis.g.append("text")
             .text("Season " + vis.group[0].season_number)
             .attr("id", "season-" + vis.group[0].season_number + "-label")
@@ -248,11 +306,26 @@ BarChart.prototype.updateVis = function() {
             .attr("text-anchor", "middle")
             .attr("color", "black")
             .attr("x", vis.linearXScale((vis.group[vis.group.length - 1].chart_index + vis.group[0].chart_index + 1)/2))
-            .attr("y", vis.y(0) + 20);
+            .attr("y", vis.y(0) + 20)
+            .attr("opacity", 0.0);
+
+        if(vis.seasonChart == false) {
+            vis.addSeasonAverageLine(vis.group);
+        }
     }
+}
 
+BarChart.prototype.addSeasonAverageLine = function(group) {
+    var vis = this;
 
-
+    vis.seasonLine = vis.g.append("line")
+        .attr("x1", vis.linearXScale(vis.group[0].chart_index))
+        .attr("x2", vis.linearXScale(vis.group[vis.group.length - 1].chart_index + 1))
+        .attr("y1", vis.y(vis.average))
+        .attr("y2", vis.y(vis.average))
+        .attr("class", "season-avg-line")
+        .attr("stroke", "black")
+        .style("stroke-dasharray", ("3, 3"));
 }
     
     
--- static/js/main.js
+++ static/js/main.js
@@ -10,10 +10,11 @@ function updateShow() {
 	$.get( "/get_show?show_id=" + showId ).then (response => {
 		// Update chart here once that code has been adapted to be object oriented
 		response = JSON.parse(response);
+
 		barChart.wrangleData(response);
+		seasonChart.wrangleData(response);
 	});
 }
-
 
 var defaultShow = 'Breaking Bad';
 var barChart;
@@ -21,5 +22,6 @@ var barChart;
 var showId = $("#show-select").find(`:contains(${defaultShow})`).attr('id').substring(5);
 $.get( "/get_show?show_id=" + showId ).then (response => {
 	response = JSON.parse(response);
-	barChart = new BarChart('#show-bar-chart', response);
+	barChart = new BarChart('#show-bar-chart', response, [700, 0.85*700], "episode-bar", false);
+	seasonChart = new BarChart('#season-bar-chart', response, [400, 0.75*400], "season-bar", true);
 });
